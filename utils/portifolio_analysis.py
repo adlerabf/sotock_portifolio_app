e20755fbe7eb5aa089f16103c0b5cae0a8a80b0f
adlerabf <adler.abf@gmail.com>
2025-04-18 17:41:40 -0300

    Requirements and files adjustments

--- utils/portifolio_analysis.py
+++ utils/portifolio_analysis.py
@@ -1,24 +1,3 @@
-import pandas as pd
-
-# def calculate_portfolio_performance(price_df, weights):
-#     """
-#     Calculate cumulative portfolio returns based on asset weights.
-#     """
-#     # Ensure index is datetime
-#     if not isinstance(price_df.index, pd.DatetimeIndex):
-#         price_df.index = pd.to_datetime(price_df.index)
-
-#     # Calculate daily percentage returns
-#     daily_returns = price_df.pct_change().dropna()
-
-#     # Weighted returns
-#     portfolio_daily_returns = (daily_returns * weights).sum(axis=1)
-
-#     # Cumulative returns
-#     cumulative_returns = (1 + portfolio_daily_returns).cumprod()
-
-#     return cumulative_returns
-
 def calculate_portfolio_performance(data, weights):
     """
     Calculates daily and cumulative returns of a portfolio based on asset weights.
